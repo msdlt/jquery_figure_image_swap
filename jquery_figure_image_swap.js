ecfbb81b8cb581190bbf382d54c4dd14d5b6041c
whanrott <eart0208@nexus.ox.ac.uk>
2016-09-09 16:24:13 +0100

    changed to bootstrap css class names; 0 position button added

--- jquery_figure_image_swap.js
+++ jquery_figure_image_swap.js
@@ -2,38 +2,34 @@
 // prepare figures
 
 $("figure.rollover").ready(function () {
-  $("figure.rollover").removeClass("hideImage");
+  $("figure.rollover").removeClass("hidden");
   //create a <ul> tag for the links to show hide
   $("figure.rollover").append("<ul></ul>");
   $("figure > figure").each(function () {
     $(this).attr("id", "targetimg_" + $(this).parent().index() + "_" + $(this).index());
-    if ($(this).index() === 0) {
-      $(this).addClass("showImage");
-      $(this).removeClass("hideImage");
+    if ($(this).index() == 0) {
+      $(this).addClass("show").removeClass("hidden");
+      // create buttons for each figure:
+      // 1. create list item with bootstrap class for btn
+      // 2. name the button based on the image alt text
+      // suggested alt for starting image (position 0) is "Clear Markings"
+      $(this).siblings("ul").append("<li class='btn btn-default'>" + $(this).children("img").attr("alt") + "</li>");
     } else {
-      $(this).addClass("hideImage");
-      $(this).removeClass("showImage");
-      // create buttons for each figure
-      // name each by its image alt text
-      var $buttonLabel = $(this).children("img").attr("alt");
-      //var $buttonIndex = $(this).index();
-      // add links to <ul> which users will click on to show/hide images
-      //$(this).siblings("ul").append("<li id='targetlink_" + $(this).parent().index() + "_" + $(this).index() + "' class='btn btn-default'>" + $buttonLabel + "</li>");
-      $(this).siblings("ul").append("<li class='btn btn-default'>" + $buttonLabel + "</li>");
+      $(this).addClass("hidden").removeClass("show");
+      // repeat button naming for all other buttons (as above)).
+      $(this).siblings("ul").append("<li class='btn btn-default'>" + $(this).children("img").attr("alt") + "</li>");
     }
   });
 
   $("figure.rollover > ul > li").click(function () {
-    //console.log("clicked" + $(this).attr("id"));
-    //console.log($(this).parents("figure.rollover").index());
     // the following statements have been split to enhance readability
     // find the index positioning of where the user has clicked
     var $buttonIndex = $(this).index();
     // find the index position of the associated image
     var $imageIndex = $(this).parents("figure.rollover").children("figure").eq($buttonIndex);
     // apply classes to show the assocated image
-    $imageIndex.removeClass("hideImage").addClass("showImage");
+    $imageIndex.removeClass("hidden").addClass("show");
     // apply classes to hide the siblings of the associated image
-    $imageIndex.siblings("figure").addClass("hideImage").removeClass("showImage");
+    $imageIndex.siblings("figure").addClass("hidden").removeClass("show");
   });
 });
